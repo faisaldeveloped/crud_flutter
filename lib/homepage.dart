6e3d000cac19b7413484e7ca88d7143575cc8500
1510is10l <faisalhussainalenazi@gmail.com>
2022-11-05 00:37:53 +0300

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mystore/loginform.dart';
+import 'listdata.dart';
+
+class HomePage extends StatelessWidget {
+  // final String text;
+  // HomePage({Key key, @required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //appBar: AppBar(title: Text("Homepage"),),
+        body: Padding(
+      padding: EdgeInsets.all(15),
+      child: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+          ),
+          Image(image: AssetImage("images/dashboard.png")),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 30,
+            width: double.infinity,
+            child: Text(
+              "Selamat datang, admin!",
+              style: TextStyle(fontFamily: "Netflix", fontSize: 18),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 50,
+            width: double.infinity,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.pink,
+                ),
+                child: Text(
+                  "Lihat Data",
+                  style: TextStyle(fontFamily: "Netflix", color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) {
+                    return Home();
+                  }));
+                }),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 50,
+            width: double.infinity,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.blue,
+                ),
+                child: Text(
+                  "Logout",
+                  style: TextStyle(fontFamily: "Netflix", color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) {
+                    return LoginForm();
+                  }));
+                  logout(context);
+                }),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+void logout(BuildContext context) {
+  final scaffold = ScaffoldMessenger.of(context);
+  scaffold.showSnackBar(
+    SnackBar(
+      content: Text("Anda berhasil logout!"),
+      action:
+          SnackBarAction(label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+    ),
+  );
+}
